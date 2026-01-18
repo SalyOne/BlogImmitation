e38aa629896d0ef87209e42417bbebad7177ff47
Salome Batkhadze <salome.batkhadze@gmail.com>
2026-01-18 18:02:41 +0400

    add: about page

---
+++ src/app/features/pages/about/about.routes.ts
@@ -0,0 +1,31 @@
+import {Routes} from '@angular/router';
+import {PostList} from './post-list';
+import {PostDetails} from '../post-details/post-details';
+import {postDetailsResolver} from '../post-details/post-details-resolver';
+import {AddEditPost} from '../add-edit-post/add-edit-post';
+import {formControlGuard} from '../../../core/guards/form-control-guard';
+
+export const POST_ROUTES: Routes = [
+  {
+    path: '',
+    component: PostList,
+  },
+  {
+    path: 'add',
+    component: AddEditPost,
+    canDeactivate: [formControlGuard],
+
+  },
+  {
+    path: ':id',
+    component: PostDetails,
+    resolve: {post: postDetailsResolver}
+  },
+  {
+    path: ':id/edit',
+    component: AddEditPost,
+    resolve: {post: postDetailsResolver},
+    canDeactivate: [formControlGuard],
+
+  },
+]
